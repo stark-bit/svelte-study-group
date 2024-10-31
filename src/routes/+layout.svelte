1f229f36d32855f0d16c430c8dff26c4863edb4a
Rei Kromulis <reinis.kromulis@ivendi.com>
2024-10-31 12:43:49 +0200

    feat: add view transition. add last slide

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,65 @@
 <script lang="ts">
 	import '../app.css';
-  import '$lib/cat';
+	import '$lib/cat';
 	let { children } = $props();
-  import { page } from '$app/stores'
+	import { page } from '$app/stores';
 
+	import { onNavigate } from '$app/navigation';
 
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
-<div class='flex bg-green-500 p-4 font-black justify-between'>
-<a href="/"><h1 class="">iVendi FE Study group</h1></a>
-  <span class=''>{$page.route.id?.slice(1)?.toUpperCase()} </span>
+<div class="flex justify-between bg-green-500 p-4 font-black">
+	<a href="/"><h1 class="">iVendi FE Study group</h1></a>
+	<span class="">{$page.route.id?.slice(1)?.toUpperCase()} </span>
 </div>
 <main class="prose mx-auto py-[4rem]">
-  {@render children()}
+	{@render children()}
 </main>
 
+<style>
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(30px);
+		}
+	}
+
+	@keyframes slide-to-left {
+		to {
+			transform: translateX(-30px);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation:
+			90ms cubic-bezier(0.4, 0, 1, 1) both fade-out,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-to-left;
+	}
+
+	:root::view-transition-new(root) {
+		animation:
+			210ms cubic-bezier(0, 0, 0.2, 1) 90ms both fade-in,
+			300ms cubic-bezier(0.4, 0, 0.2, 1) both slide-from-right;
+	}
+</style>
--- src/routes/gipity/models/+page.svelte
+++ src/routes/gipity/models/+page.svelte
@@ -12,8 +12,12 @@
 
 <h2>Parameters</h2>
 <h3>
-	Size does matter, but it's <a target="_blank" href="https://arxiv.org/pdf/2410.05229">debatable</a
+	Size does matter, but it's <a
+		target="_blank"
+		href="https://notebooklm.google.com/notebook/e6982cd5-30c5-4dd2-8fad-3aeb7624a4bd/audio"
+		>debatable</a
 	>
+	based on this paper - <a target="_blank" href="https://arxiv.org/pdf/2410.05229">paper</a>
 </h3>
 <ul>
 	<li><strong>GPT-4</strong> rumored to have 170 trillion</li>
@@ -21,19 +25,40 @@
 	<h4>Open sourced*</h4>
 	<li>Llama 3.1 1B - 420B</li>
 	<h4>Sweet spot</h4>
-  <li>13B - 32B</li>
+	<li>13B - 32B</li>
 </ul>
 
 <h2>Quantisation</h2>
 <ul>
 	<li>It's a compression technique</li>
 	<h3>
-		Why e.g. <a
-			target="_blank"
-			href="https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF/tree/main"
+		Why e.g. <a target="_blank" href="https://huggingface.co/meta-llama/Llama-3.1-8B/tree/main"
 			>Llama-3.1.8B</a
 		>
 	</h3>
-	<li>8B parameters ~ 8GB of memory needed</li>
-  <li>GPU RAM </li>
+	<li>8B parameters ~ 15GB of memory needed</li>
+	<li>
+		<a
+			target="_blank"
+			href="https://www.reddit.com/r/LocalLLaMA/comments/178el7j/comment/k50vs5v/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button"
+			>Safetensor vs GGUF vs GGML vs Other</a
+		>
+	</li>
+	<li>
+		<a href="https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF">Q2 vs Q5 vs f32</a>
+	</li>
 </ul>
+
+<h2>
+	<a href="https://www.gpu-mart.com/blog/custom-llm-models-with-ollama-modelfile" target="_blank">
+		Modelfiles
+	</a>
+</h2>
+<pre>ollama show --modelfile llama3.2:1B</pre>
+or send it to txt file
+<pre>ollama show --modelfile llama3.2:1B > ~/Downloads/llama3.2.txt </pre>
+
+<h2>How small can you go?</h2>
+<ul><li><a href="https://apps.apple.com/us/app/pocketpal-ai/id6502579498">Apple PocketPal</a>
+  </li><li><a href="https://play.google.com/store/apps/details?id=com.pocketpalai&hl=en" target="_blank">Android PocketPal</a></li></ul>
+
